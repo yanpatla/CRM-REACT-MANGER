b1b2a96c8c6fda3a3511a740c4e6493fabddf297
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2022-01-17 18:01:29 +0200

    Links

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="md:flex md:min-h-screen">
-        <div className="md:w-1/4">
-            1
-        </div>
-        <div className="md:w-3/4">
+      <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">
+          CRM - Clients
+        </h2>
+
+        <nav className="mt-10">
+          <Link
+            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            to="/clients"
+          >
+            Clients
+          </Link>
+          <Link
+            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            to="/clients/new"
+          >
+            New Clients
+          </Link>
+        </nav>
+      </div>
+      <div className="md:w-3/4">
         <Outlet />
-        </div>
- 
+      </div>
     </div>
   );
 };
