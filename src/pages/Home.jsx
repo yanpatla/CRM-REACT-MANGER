9a6e05558b86c7084a4e5a3951cf414b35b22144
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2022-01-18 12:35:58 +0200

    finish the layout of the Table and add the Clients

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Clients from "../components/Clients";
 
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+  const [clients, setClients] = useState([]);
+  useEffect(() => {
+    const getClients = async () => {
+      try {
+        const url = "http://localhost:4000/clients";
+        const res = await axios.get(url);
+        setClients(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getClients();
+  }, []);
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900"> Clients</h1>
 
-export default Home
+      <p className="mt-3">Manage your Clients</p>
+      <table className="w-full mt-5 table-auto shadow bg-white">
+        <thead className="bg-blue-800 text-white">
+          <tr>
+            <th className="p-2">Name</th>
+            <th className="p-2">Contact</th>
+            <th className="p-2">Company</th>
+            <th className="p-2">Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {clients.map((client) => (
+            <Clients key={client.id} client={client} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Home;
